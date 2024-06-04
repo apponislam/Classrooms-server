68a36e3f0a38bba2c5c45180904e18b7f274b868
apponislame <11appon11@gmail.com>
2024-06-05 01:34:09 +0600

    All Classes Showing for users

--- index.js
+++ index.js
@@ -144,6 +144,20 @@ async function run() {
             res.send(result);
         });
 
+        app.get("/Classes/Approved/Home", async (req, res) => {
+            const query = { status: "approved" };
+            const cursor = allClasses.find(query).sort({ enroll: -1 });
+            const result = await cursor.toArray();
+            res.send(result);
+        });
+
+        app.get("/Classes/Approved", async (req, res) => {
+            const query = { status: "approved" };
+            const cursor = allClasses.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+        });
+
         app.get("/Classes/:id", async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) };
